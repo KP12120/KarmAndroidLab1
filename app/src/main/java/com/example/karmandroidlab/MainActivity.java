53093d4412c82691ab935a91d12b8218a2edbd6c
karmp <abcd>
2021-09-24 15:59:37 -0400

    inital commit

--- app/src/main/java/com/example/karmandroidlab/MainActivity.java
+++ app/src/main/java/com/example/karmandroidlab/MainActivity.java
@@ -9,7 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -64,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        //Imagebutton
+        //Image button
         ImageButton n = findViewById( R.id.myimagebutton );
         n.setOnClickListener(new View.OnClickListener() {
             @Override
